e1a5e3c48175c897d44b2e0ffcaaf3e0fd35070a
Rato Kuzmanić <rato.kuzmanic@live.com>
2015-10-15 23:35:50 +0200

    Added solution to the first assignment

---
+++ Assignment 01/Assignment 01.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+typedef struct Student {
+	char FirstName[50];
+	char LastName[50];
+	int Score;
+};
+
+int CountRows(char*);
+int EnlistStudents(char*, Student*, int);
+void OutputData(Student*, int, int);
+
+int main(void)
+{
+	int numberOfRows, maxScore;
+
+	Student* _students;
+
+	numberOfRows = CountRows("rezultati.txt");
+	if(numberOfRows == -1)
+	{
+		printf("Problem kod citanja datoteke.\n");
+		return 0;
+	}
+	_students = (Student*)malloc(sizeof(Student) * numberOfRows);
+
+	maxScore = EnlistStudents("rezultati.txt", _students, numberOfRows);
+	if(maxScore == -1)
+	{
+		printf("Problem kod citanja datoteke.\n");
+		return 0;
+	}
+	else if(maxScore == 0)
+	{
+		printf("Ne mozemo dijeliti s nula, stoga ne vrijedi relativno ocijenjivanje.\n");
+		return 0;
+	}
+
+	OutputData(_students, numberOfRows, maxScore);
+
+	return 0;
+}
+
+int CountRows(char* fileName) 
+{
+	FILE *file;
+	char buffer[51];
+	int numberOfRows = 0;
+
+	file = fopen(fileName, "r");
+	if(file == NULL) return -1;
+	
+	while(!feof(file))
+	{
+		fgets(buffer, 50, file);
+		if(strlen(buffer) > 0)
+			numberOfRows++;
+	}
+
+	fclose(file);
+	return numberOfRows;
+}
+
+int EnlistStudents(char* fileName, Student* _students, int numberOfStudents)
+{
+	FILE *file;
+	int counter = 0;
+	int maxScore = 0;
+	
+	file = fopen(fileName, "r");
+	if(file == NULL) return -1;
+
+	while(!feof(file))
+	{
+		fscanf(file, "%s %s %d", _students[counter].FirstName, _students[counter].LastName, &_students[counter].Score);
+
+		if(_students[counter].Score > maxScore)
+			maxScore = _students[counter].Score;
+
+		counter++;
+	}
+
+	fclose(file);
+	return maxScore;
+}
+
+void OutputData(Student* _students, int numberOfStudents, int maxScore)
+{
+	int i;
+	for(i = 0; i < numberOfStudents; i++)
+		printf("%s \t %s \t %d \t %.2f\n", _students[i].FirstName, _students[i].LastName, _students[i].Score, (float)_students[i].Score/maxScore * 100.0);
+}
