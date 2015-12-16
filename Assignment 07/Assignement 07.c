e5d6845496e6c0fb56485ed6005b40f6e4d39ec7
Rato Kuzmanić <rato.kuzmanic@live.com>
2015-12-16 12:51:23 +0100

    Added presolved solution to the seventh assignment
    Solution has not yet been reviewed by assistant professor and will be
    updated once it is reviewed and if needed revised

---
+++ Assignment 07/Assignement 07.c
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define BUFFER_MAX_SIZE 4096
+#define FILE_NAME "postfix.txt"
+
+typedef struct Element {
+	int Value;
+	Element* Next;
+};
+
+typedef struct Element* Node;
+
+int IsListEmpty(Node);
+int IsLastElement(Node);
+int IsOutOfRange(Node);
+
+int IsCharacterNumber(char);
+int IsValidOperation(char);
+int ConvertCharNumberToInt(char);
+
+Node CreateHeadNode();
+Node CreateNode(int);
+Node GetNextElement(Node);
+Node GetLastElement(Node);
+int* GetValuesAndDeleteFirstNElements(Node, int);
+
+void InsertElement(Node, Node);
+void AddElementToListStart(Node, Node);
+void DeleteFirstElementOfList(Node);
+void DeleteAllElements(Node);
+
+void Push(Node, int);
+void Pop(Node);
+
+void ExecuteOperationOnStack(Node, char);
+void CalculatePostfixTerm(Node, char*);
+
+char* ReadFirstLineInFile(char*);
+
+void PrintListElement(Node);
+void PrintAllListElements(Node);
+
+int main()
+{
+	Node _head = CreateHeadNode();
+
+	char* testInput = ReadFirstLineInFile(FILE_NAME);
+	if (testInput != NULL) 
+	{
+		CalculatePostfixTerm(_head, testInput);
+
+		PrintAllListElements(_head);
+		DeleteAllElements(_head);
+	}
+	else
+		printf("Datoteka %s ne postoji.\n", FILE_NAME);
+
+	free(_head);
+
+	return 0;
+}
+
+int IsListEmpty(Node _head)
+{
+	return _head->Next == NULL;
+}
+
+int IsLastElement(Node _element)
+{
+	return _element->Next == NULL;
+}
+
+int IsOutOfRange(Node _element)
+{
+	return _element == NULL;
+}
+
+int IsCharacterNumber(char character)
+{
+	return	character >= '0' &&
+			character <= '9';
+}
+
+int IsValidOperation(char operation)
+{
+	return	operation == '+' ||
+			operation == '-' ||
+			operation == '*' ||
+			operation == '/';
+}
+
+int ConvertCharNumberToInt(char character)
+{
+	return character - '0';
+}
+
+Node CreateHeadNode()
+{
+	Node _newHead = (Node)malloc(sizeof(Element));
+	if (IsOutOfRange(_newHead)) return NULL;
+
+	_newHead->Next = NULL;
+	return _newHead;
+}
+
+Node CreateNode(int value)
+{
+	Node _newNode = (Node)malloc(sizeof(Element));
+	if (IsOutOfRange(_newNode)) return NULL;
+
+	_newNode->Value = value;
+	_newNode->Next = NULL;
+
+	return _newNode;
+}
+
+Node GetNextElement(Node _element)
+{
+	return _element->Next;
+}
+
+Node GetLastElement(Node _head)
+{
+	Node _lastElement = _head;
+
+	while (!IsLastElement(_lastElement))
+		_lastElement = GetNextElement(_lastElement);
+
+	return _lastElement;
+}
+
+int* GetValuesAndDeleteFirstNElements(Node _head, int numberOfElements)
+{
+	int i;
+	int* firstNElements = (int*)malloc(sizeof(int) * numberOfElements);
+
+	for (i = 0; i < numberOfElements; i++)
+	{
+		firstNElements[i] = GetNextElement(_head)->Value;
+		Pop(_head);
+	}
+
+	return firstNElements;
+}
+
+void InsertElement(Node _previous, Node _next)
+{
+	_next->Next = GetNextElement(_previous);
+	_previous->Next = _next;
+}
+
+void AddElementToListStart(Node _head, Node _element)
+{
+	InsertElement(_head, _element);
+}
+
+void DeleteFirstElementOfList(Node _head)
+{
+	Node _firstElement = GetNextElement(_head);
+
+	_head->Next = GetNextElement(_firstElement);
+
+	free(_firstElement);
+}
+
+void DeleteAllElements(Node _head)
+{
+	while (!IsListEmpty(_head))
+		DeleteFirstElementOfList(_head);
+}
+
+void Push(Node _head, int value)
+{
+	AddElementToListStart(_head, CreateNode(value));
+}
+
+void Pop(Node _head)
+{
+	if (!IsListEmpty(_head))
+		DeleteFirstElementOfList(_head);
+}
+
+void ExecuteOperationOnStack(Node _head, char operation)
+{
+	int* operationElements = GetValuesAndDeleteFirstNElements(_head, 2);
+	switch (operation)
+	{
+	case '+':
+		Push(_head, operationElements[0] + operationElements[1]);
+		break;
+	case '-':
+		Push(_head, operationElements[0] - operationElements[1]);
+		break;
+	case '*':
+		Push(_head, operationElements[0] * operationElements[1]);
+		break;
+	case '/':
+		Push(_head, operationElements[0] / operationElements[1]);
+		break;
+	}
+}
+
+void CalculatePostfixTerm(Node _head, char* term)
+{
+	int i;
+	for (i = 0; term[i]; i++)
+	{
+		if (IsCharacterNumber(term[i]))
+			Push(_head, ConvertCharNumberToInt(term[i]));
+		else if (IsValidOperation(term[i]))
+			ExecuteOperationOnStack(_head, term[i]);
+	}
+}
+
+char* ReadFirstLineInFile(char* fileName)
+{
+	FILE* listLog;
+	char buffer[BUFFER_MAX_SIZE];
+
+	listLog = fopen(fileName, "r");
+	if (listLog == NULL) return NULL;
+
+	fgets(buffer, BUFFER_MAX_SIZE, listLog);
+
+	return buffer;
+}
+
+void PrintListElement(Node _element)
+{
+	printf("%d\n", _element->Value);
+}
+
+void PrintAllListElements(Node _head)
+{
+	if (IsListEmpty(_head))
+	{
+		printf("Lista je prazna.\n");
+		return;
+	}
+
+	Node _currentElement = GetNextElement(_head);
+	while (!IsOutOfRange(_currentElement))
+	{
+		PrintListElement(_currentElement);
+		_currentElement = GetNextElement(_currentElement);
+	}
+}
